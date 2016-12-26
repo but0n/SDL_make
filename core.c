560f3ce97569a36bd7805c5105d806816d008235
but0n <but0n@icloud.com>
2016-12-26 17:39:50 +0800

    Remove Needn't

--- core.c
+++ core.c
@@ -3,16 +3,14 @@
 #include <stdlib.h> /* for atexit() */
 
 int main(int argc, char** argv) {
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER)) {
         fprintf(stderr,
                 "\nUnable to initialize SDL:  %s\n",
                 SDL_GetError()
                );
         return 1;
     }
-    // atexit(SDL_Quit);
 	SDL_Quit();
-
 
     return 0;
 }
