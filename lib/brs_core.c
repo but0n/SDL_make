7480badc6ee4d60a49bc6e86b5819103d765e3cc
but0n <but0n@icloud.com>
2017-10-20 19:01:05 +0800

    feat: (Fade-in&Fade-out)

--- lib/brs_core.c
+++ lib/brs_core.c
@@ -19,14 +19,15 @@ int main() {
     tmp2.image_path = "../Resources/hello.png";
     // tmp.position = {0,0};
     // tmp.pivot_offset = {0,0};
-    tmp2.position.x = 800;
-    tmp2.position.y = 800;
+    tmp2.position.x = 0;
+    tmp2.position.y = 1000;
     tmp2.pivot_offset.x = 256;
     tmp2.pivot_offset.y = 256;
     tmp2.z_index = 1;
 
     brs_sprite_t map = {
-        .image_path = "../Resources/test_background.png"
+        .image_path = "../Resources/test_background.png",
+        .z_index = -1
     };
     brs_AddSprite(&map, 0);
 
@@ -35,10 +36,15 @@ int main() {
 
     brs_AddSprite(&tmp, 0);
 
-    int speed = 5; // 3 pixel per frame
+    int force = 4; // 3 pixel per frame
     for(int i = 0; i < 100; i++) {
         brs_RenderScene(0);
-        brs_CameraPosition(0)->x += speed;
+        brs_CameraPosition(0)->x -= i*i/1000*force;
+        brs_Delay(100/6);
+    }
+    for(int i = 100; i > 0; i--) {
+        brs_RenderScene(0);
+        brs_CameraPosition(0)->x -= i*i/1000*force;
         brs_Delay(100/6);
     }
 
